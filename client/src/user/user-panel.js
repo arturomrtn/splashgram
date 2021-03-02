23fa0d9434f621a6e5af7e0677fec6c8e85be899
Arturo Moretón Arrillaga <arturomoretonarrillaga@Air-de-Arturo.home>
2021-03-02 17:34:48 +0100

    setting up frontend

---
+++ client/src/user/user-panel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class UserPanel extends Component {
+    render() {
+        return (
+            <div className="user-panel">
+                hi
+            </div>
+        )
+    }
+}
