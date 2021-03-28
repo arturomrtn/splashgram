b2526350052c30c896b7aad58a27a8d39363e879
Arturo <moreton.arturo@gmail.com>
2021-03-28 16:32:39 +0200

    deploying to heroku

--- server/configs/mongoose.config.js
+++ server/configs/mongoose.config.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose')
-const URI = process.env.DB_LOCAL
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 mongoose
-    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-    .then(x => console.log('>>> Database Connected! <<'))
-    .catch(err => console.error('Error connecting to mongo', err))
+    .connect(process.env.DB_REMOTE, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(details => {
+        const { name, client } = details.connections[0]
+        console.log(`Connected to database "${name}" (URL: ${client.s.url})`)
+    })
+    .catch(err => console.error('Error connecting to Mongo', err))
 
-module.exports = mongoose
+
+
+
+
+    //const mongoose = require('mongoose')
+//const URI = process.env.DB_LOCAL
+
+//mongoose
+  //  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+    //.then(x => console.log('>>> Database Connected! <<'))
+    //.catch(err => console.error('Error connecting to mongo', err))
+
+//module.exports = mongoose
